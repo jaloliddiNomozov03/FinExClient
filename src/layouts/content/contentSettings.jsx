aed08a0dbe6a832b0b5021f7ae6bb5de2e26c2af
Mirzohid22 <smm2226fstd@gmail.com>
2021-08-19 00:58:19 +0500

    in Settings Menu created Menu of AccessBase

--- src/layouts/content/contentSettings.jsx
+++ src/layouts/content/contentSettings.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ContentSlider from "../../components/ComponentSlider/slider";
 import { Switch, Route } from "react-router-dom";
 import ContentKonstantalar from "../../components/settings/konstantalar/konstantalar";
+import AccessContent from "../../components/settings/access/access";
 
 class ContentSettings extends React.Component {
   render() {
@@ -12,6 +13,9 @@ class ContentSettings extends React.Component {
         </Route>
         <Route path="/Settings/konstantalar">
             <ContentKonstantalar />
+        </Route>
+        <Route path="/Settings/access">
+            <AccessContent />
         </Route>
       </Switch>
     );
